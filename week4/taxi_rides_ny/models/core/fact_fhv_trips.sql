f80d3ea428eac5ccfeb70f3a04487720f5c04a0b
evhlevine@gmail.com <evhlevine@gmail.com>
2023-02-27 06:04:15 +0000

    update

--- week4/taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ week4/taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -10,16 +10,16 @@ select
     fhv_data.pickup_datetime,
     fhv_data.dropoff_datetime,
     fhv_data.pickup_locationid,
-    -- pickup_zone.borough as pickup_borough, 
-    -- pickup_zone.zone as pickup_zone, 
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone, 
     fhv_data.dropoff_locationid,
-    -- dropoff_zone.borough as dropoff_borough, 
-    -- dropoff_zone.zone as dropoff_zone,  
+    dropoff_zone.borough as dropoff_borough, 
+    dropoff_zone.zone as dropoff_zone,  
     fhv_data.SR_Flag,
     fhv_data.Affiliated_base_number 
 from {{ ref('stg_fhv_tripdata') }} as fhv_data
 
--- inner join dim_zones as pickup_zone
--- on fhv_data.pickup_locationid = pickup_zone.locationid
--- inner join dim_zones as dropoff_zone
--- on fhv_data.dropoff_locationid = dropoff_zone.locationid
+inner join dim_zones as pickup_zone
+on fhv_data.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_data.dropoff_locationid = dropoff_zone.locationid
