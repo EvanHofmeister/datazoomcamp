275e841458fd13ff28ae109087c49afcb7791d04
EvanHofmeister <evan.hofmeister@gmail.com>
2023-03-16 10:21:41 -0500

    Update python-kafka_consumer.py

--- week6/python-kafka_consumer.py
+++ week6/python-kafka_consumer.py
@@ -1,32 +1,37 @@
-import pandas as pd
+from kafka import KafkaConsumer
 import json
-import datetime as dt
-from time import sleep
-from kafka import KafkaProducer
+import pandas as pd
+import logging
+import sys
 
-producer = KafkaProducer(bootstrap_servers=['localhost:PORT'])
-# Initialize 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+root = logging.getLogger()
+root.setLevel(logging.INFO)
 
-path = r'/Users/evanhofmeister/data-zoom-camp/Data'/green_tripdata_2019-01.csv.gz'
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
-for count, chunk in enumerate(pd.read_csv(path, compression="gzip", chunksize=10000)):
-    # Fix format
+consumer = KafkaConsumer('trips',
+                         group_id=None,
+                         bootstrap_servers=['localhost:PORT'],
+                         value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+                         auto_offset_reset='earliest',
+                         enable_auto_commit=False, api_version=(1, 4, 1))
 
-    chunk["pickup_datetime"] = pd.to_datetime(chunk["pickup_datetime"])
-    chunk["dropoff_datetime"] = pd.to_datetime(chunk["dropOff_datetime"])
+df = pd.DataFrame()
+for count, message in enumerate(consumer):
+    print('test')
+    df = pd.concat([df, pd.DataFrame(message.value)], axis=0)
+    logging.info(f"====>>>>>: {str(msg)}")
 
-    chunk = chunk[['PUlocationID', 'pickup_datetime', 'dropoff_datetime']]
+    
+    print(count, df)
+        
+    # Count frequency of PUlocationID.
+# print(df.head())
+summary = df['PUlocationID'].value_counts()
 
-    key = str(count).encode()
-
-    chunkd = chunk.to_dict()
-
-    # Encode the dictionary into JSON 
-    data = json.dumps(chunkd, default=str).encode('utf-8')
-
-    # Send the data to Kafka
-    producer.send(topic="taxi_trips", key=key, value=data)
-
-    # Sleep
-    sleep(0.3)
+print(summary)
