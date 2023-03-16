5fc9ce39a4e8d0d59fff36a4e9e72ba3dabf314c
EvanHofmeister <evan.hofmeister@gmail.com>
2023-03-16 10:22:36 -0500

    Update python-kafka_consumer.py

--- week6/python-kafka_consumer.py
+++ week6/python-kafka_consumer.py
@@ -1,6 +1,10 @@
-from kafka import KafkaConsumer
-import json
+# Import packages
 import pandas as pd
+import numpy as np
+import json
+import datetime as dt
+from time import sleep
+from kafka import KafkaProducer
 import logging
 import sys
 
@@ -14,24 +18,27 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-consumer = KafkaConsumer('trips',
-                         group_id=None,
-                         bootstrap_servers=['localhost:PORT'],
-                         value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-                         auto_offset_reset='earliest',
-                         enable_auto_commit=False, api_version=(1, 4, 1))
+producer = KafkaProducer(bootstrap_servers=['localhost:PORT'])
+# Initialize 
+path = r'/Users/evanhofmeister/data-zoom-camp/Data'/green_tripdata_2019-01.csv.gz'
 
-df = pd.DataFrame()
-for count, message in enumerate(consumer):
-    print('test')
-    df = pd.concat([df, pd.DataFrame(message.value)], axis=0)
-    logging.info(f"====>>>>>: {str(msg)}")
+for count, chunk in enumerate(pd.read_csv(path, compression="gzip", chunksize=10000)):
+    # Fix format
 
-    
-    print(count, df)
-        
-    # Count frequency of PUlocationID.
-# print(df.head())
-summary = df['PUlocationID'].value_counts()
+    chunk["pickup_datetime"] = pd.to_datetime(chunk["pickup_datetime"])
+    chunk["dropoff_datetime"] = pd.to_datetime(chunk["dropOff_datetime"])
 
-print(summary)
+    chunk = chunk[['PUlocationID', 'pickup_datetime', 'dropoff_datetime']]
+
+    key = str(count).encode()
+
+    chunkd = chunk.to_dict()
+
+    # Encode the dictionary into JSON 
+    data = json.dumps(chunkd, default=str).encode('utf-8')
+
+    # Send the data to Kafka
+    producer.send(topic="trips", key=key, value=data)
+
+    # Sleep
+    sleep(0.3)
